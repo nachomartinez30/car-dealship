a959b7a9e425abbf7935f7b03cf65425bf94ab3e
nachomartinez30 <nachomartinez3010@gmail.com>
2023-08-30 18:54:41 -0600

    inyeccion de dependencias

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,15 +1,20 @@
+import { CarsService } from './cars.service';
 import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
-  private cars = ['toyota', 'honda', 'jeep'];
+
+  constructor(
+    private readonly carsService: CarsService
+  ) { }
+
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAll();
   }
 
   @Get(':id')
   getCarByID(@Param('id') id: number) {
-    return this.cars[id];
+    return this.carsService;
   }
 }
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+    private cars = [
+        { id: 0, brand: 'toyota', model: 'Rav4' },
+        { id: 1, brand: 'honda', model: 'Civic' },
+        { id: 2, brand: 'jeep', model: 'Compass' },
+    ];
+    findAll() {
+        return this.cars;
+    }
+}
